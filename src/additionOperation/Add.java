a4c35d54b792c925509e9693290813b6d571c12c
Admin <Admin@DESKTOP-IFQPFM4>
2021-08-10 15:50:11 -0700

    Addition Program

---
+++ src/additionOperation/Add.java
@@ -0,0 +1,12 @@
+package additionOperation;
+
+public class Add
+{
+	public static void main(String[] args) 
+	{
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("Addition is "+c);
+	}
+}
