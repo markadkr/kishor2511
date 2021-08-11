99a5c36d261b02371f472c7e2764846aa48fed43
Admin <Admin@DESKTOP-IFQPFM4>
2021-08-10 20:04:19 -0700

    changes made

--- src/hello/hellosystem.java
+++ src/hello/hellosystem.java
@@ -1,7 +1,10 @@
 package hello;
 
-public class hellosystem {
-public static void main(String[] args) {
+public class hellosystem 
+{
+public static void main(String[] args) 
+{
 	System.out.println("Hello Kishor");
+	System.out.println("Hi Pradip");
 }
 }
